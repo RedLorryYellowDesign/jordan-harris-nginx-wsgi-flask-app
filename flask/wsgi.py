0e736331ad5a5ffb062489735464a0d1f433b5d6
Jordan Harris <jordan.lee.harris@icloud.com>
2023-04-04 15:07:57 +0100

    Added Admin Section

--- flask/wsgi.py
+++ flask/wsgi.py
@@ -2,4 +2,6 @@ from app import app
 import os
 
 if __name__ == "__main__":
+    app_dir = os.path.realpath(os.path.dirname(__file__))
+    database_path = os.path.join(app_dir, app.config['DATABASE_FILE'])
     app.run(host='0.0.0.0', port=os.environ.get("FLASK_SERVER_PORT"), debug=True)
